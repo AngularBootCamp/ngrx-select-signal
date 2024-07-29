a70cf09cdce254673b9effb552a73fa41320579f
Zachary Kipping <zachary.kipping@oasisdigital.com>
2024-07-29 11:06:37 -0500

    curriculum update

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,11 @@
 // https://github.com/stackblitz/core/issues/2366
 import 'zone.js'; // Avoid error in StackBlitz for Angular polyfill
 
-import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideEffects } from '@ngrx/effects';
-import { provideStore } from '@ngrx/store';
-import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app/app.component';
-import { AppEffects } from './app/app.effects';
-import { HomeTasksEffects } from './app/home-tasks.effects';
-import { reducers } from './app/reducers';
-import { WorkTasksEffects } from './app/work-tasks.effects';
-import { environment } from './environments/environment';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideStore(reducers, {
-      runtimeChecks: {
-        strictStateSerializability: true,
-        strictActionSerializability: true
-      }
-    }),
-    provideEffects([AppEffects, HomeTasksEffects, WorkTasksEffects]),
-    provideStoreDevtools({
-      maxAge: 50,
-      logOnly: environment.production,
-      trace: true
-    }),
-    provideHttpClient()
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err =>
+  console.error(err)
+);
